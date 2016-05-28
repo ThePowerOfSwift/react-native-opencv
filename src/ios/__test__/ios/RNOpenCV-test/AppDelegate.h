64d701d7e7f373f0a7b5c1b623961a2869cdea45
anuj_kosambi <anujkosambi@gmail.com>
2016-05-29 05:10:36 +0530

    Adding Test Files

---
+++ src/ios/__test__/ios/RNOpenCV-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RNOpenCV-test
+//
+//  Created by AnujKosambi on 29/05/16.
+//  Copyright © 2016 dadevelopers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/ios/__test__/ios/RNOpenCV-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RNOpenCV-test
+//
+//  Created by AnujKosambi on 29/05/16.
+//  Copyright © 2016 dadevelopers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
