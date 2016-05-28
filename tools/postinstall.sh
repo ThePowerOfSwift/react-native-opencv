adddaf57972376f0d450a30f405a996bb86a5e95
anuj_kosambi <anujkosambi@gmail.com>
2016-05-29 01:00:32 +0530

    Modifying pbxproj files for stable build

--- tools/postinstall.sh
+++ tools/postinstall.sh
@@ -2,6 +2,8 @@
 echo '### Installing Pod Dependencies ###'
 cd src/ios && pod install
 echo '### Linking Configurations ###'
-ln -s `pwd`/Pods/Target\ Support\ Files/Pods-RNOpenCV/Pods-RNOpenCV.debug.xcconfig RNOpenCV.debug.xcconfig
-ln -s `pwd`/Pods/Target\ Support\ Files/Pods-RNOpenCV/Pods-RNOpenCV.release.xcconfig RNOpenCV.release.xcconfig
+rm RNOpenCV.debug.xcconfig
+rm RNOpenCV.release.xcconfig
+cp  ./Pods/Target\ Support\ Files/Pods-RNOpenCV/Pods-RNOpenCV.debug.xcconfig RNOpenCV.debug.xcconfig
+cp  ./Pods/Target\ Support\ Files/Pods-RNOpenCV/Pods-RNOpenCV.release.xcconfig RNOpenCV.release.xcconfig
 echo '### Done ###'
